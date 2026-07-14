2ca99d477aea9f9fe7f77571b61d13c50e43e87e
やっつん。 <kisame294@gmail.com>
2026-07-14 15:13:51 +0900

    最新のコード状態を復元

---
+++ src/main/java/com/yattsun/minesweeperweb/MinesweeperWebApplication.java
@@ -0,0 +1,13 @@
+package com.yattsun.minesweeperweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesweeperWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yattsun/minesweeperweb/MinesweeperWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yattsun.minesweeperweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
